7d9f01e209e2caeb350a85278ed08c1239aa239c
AleksaYO <yura.aleksa.98@gmail.com>
2023-01-12 18:43:44 +0200

    someChange

--- js/modal.js
+++ js/modal.js
@@ -5,10 +5,27 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", onOpenModal);
+  refs.closeModalBtn.addEventListener("click", onCloseModal);
+  refs.modal.addEventListener("click", onClickBackdropCloseModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function onOpenModal() {
+    window.addEventListener("keydown", onEskCloseModal);
+    refs.modal.classList.remove("is-hidden");
+  }
+  function onCloseModal() {
+    window.removeEventListener("keydown", onEskCloseModal);
+
+    refs.modal.classList.add("is-hidden");
+  }
+  function onEskCloseModal(event) {
+    if (event.code === "Escape") {
+      onCloseModal();
+    }
+  }
+  function onClickBackdropCloseModal(event) {
+    if (event.currentTarget === event.target) {
+      onCloseModal();
+    }
   }
 })();
